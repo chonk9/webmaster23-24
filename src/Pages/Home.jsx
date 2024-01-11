43c76aa1c0fb286b4b643f481a7d5e2545d96611
Gideon Elliott <gideon.joseph.elliott@gmail.com>
2024-01-11 16:24:20 -0500

    Moved map to Tax Incentives page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import Navbar from '../Components/navbar';
 import Footer from '../Components/footer';
-import Map from '../Components/map';
 
 export default function Home() {
   return (<>
-    <Map />
     <h1>Home Page : )</h1>
   </>);
 }
--- src/Pages/TaxIncentives.jsx
+++ src/Pages/TaxIncentives.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../Components/navbar';
 import Footer from '../Components/footer';
+import Map from '../Components/map';
 
 export default function TaxIncentives() {
   return(<>
-    <h>Tax Solutions</h>
+    <h1>Tax Solutions</h1>
+    <Map />
   </>);
 
 }
