5d6eac3a9b71f50adde3c9ad8b7f19d8d46a8823
Gideon Elliott <gideon.joseph.elliott@gmail.com>
2024-01-11 21:19:55 -0500

    Added LeafReveal component (see home page)

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import Navbar from '../Components/navbar';
 import Footer from '../Components/footer';
+import LeafReveal from '../Components/leafReveal';
 
 export default function Home() {
+  function FunFact() {
+    return (<>
+      <h1>Did you know this?</h1>
+      This is interesting. This is interesting. This is interesting. This is interesting. This is interesting. This is interesting. This is interesting. This is interesting. This is interesting. This is interesting. This is interesting. This is interesting. This is interesting. This is interesting. This is interesting. This is interesting. This is interesting.
+    </>)
+  }
+
   return (<>
-    <h1>Home Page : )</h1>
+    <h1>Welcome to Green Guardians!</h1>
+    <LeafReveal content={FunFact}/>
+    text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />text<br />
   </>);
 }
