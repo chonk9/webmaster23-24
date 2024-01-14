716f4c92e756f828b81cc16d6cbe130eb2d8a139
Ian <austinyan54@gmail.com>
2024-01-14 18:27:04 -0500

    dodo

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,27 +1,29 @@
-import { Link, useMatch, useResolvedPath } from "react-router-dom"
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
-export default function navbar(){
-    const pathname = window.location.pathname
-    return( <nav className="nav">
-        <Link to = "/" className = "site-title">Green Guardians</Link>
-        <ul>
-            <CustomLink to = "/clean-solutions">Clean Solutions</CustomLink>
-            <CustomLink to = "/tax-incentives">Tax Incentives</CustomLink>
-            <CustomLink to = "/about-us">About Us</CustomLink>
-        </ul>
-    </nav>
-    )
+export default function navbar() {
+    const pathname = window.location.pathname;
+    return (
+        <nav className="nav">
+            <Link to="/" className="site-title">
+                Green Guardians
+            </Link>
+            <ul>
+                <CustomLink to="/clean-solutions">Solutions</CustomLink>
+                <CustomLink to="/tax-incentives">Tax</CustomLink>
+                <CustomLink to="/about-us">About Us</CustomLink>
+            </ul>
+        </nav>
+    );
 }
 
-
-function CustomLink({to, children, ...props}) {
-    const resolvedPath = useResolvedPath(to)
-    const isActive = useMatch({ path: resolvedPath.pathname, end: true})
+function CustomLink({ to, children, ...props }) {
+    const resolvedPath = useResolvedPath(to);
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true });
     return (
-        <li className = {isActive ? "active" : ""}>
-            <Link to = {to} {...props}>
+        <li className={isActive ? "active" : ""}>
+            <Link to={to} {...props}>
                 {children}
             </Link>
         </li>
-    )
+    );
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,28 +1,42 @@
-import React from 'react'
-import Navbar from '../Components/navbar';
-import Footer from '../Components/footer';
-import LeafReveal from '../Components/leafReveal';
-import TextWrap from '../Components/textWrap';
+import React, { useEffect } from "react";
+import LeafReveal from "../Components/leafReveal";
+// import TextWrap from "../Components/textWrap";
 
 export default function Home() {
-  return (<>
-    <h1>Welcome to Green Guardians!</h1>
-    <LeafReveal content={<>
-      <h2>Did you know this?</h2>
-      This is interesting. This is interesting. This is interesting. This is interesting. This is interesting. This is interesting. This is interesting. This is interesting. This is interesting. This is interesting. This is interesting. This is interesting. This is interesting. This is interesting. This is interesting. This is interesting. This is interesting.
-    </>}/>
-    
-    <h2>You can make a difference!</h2>
-    <p>[Insert some info]</p>
-    <p>[Insert some info]</p>
-    <p>[Insert some info]</p>
-    <p>[Insert some info]</p>
-    <p>[Insert some info]</p>
-    <p>[Insert some info]</p>
-    <p>[Insert some info]</p>
-    <p>[Insert some info]</p>
-    <p>[Insert some info]</p>
-    <p>[Insert some info]</p>
-    <p>[Insert some info]</p>
-  </>);
+    useEffect(() => {
+        // scroll to the top of page, fixes bug where switching to page retains scroll position
+        window.scrollTo(0, 0);
+    }, []);
+    return (
+        <>
+            <h1>Welcome to Green Guardians!</h1>
+            <LeafReveal
+                content={
+                    <>
+                        <h2>Did you know this?</h2>
+                        This is interesting. This is interesting. This is
+                        interesting. This is interesting. This is interesting.
+                        This is interesting. This is interesting. This is
+                        interesting. This is interesting. This is interesting.
+                        This is interesting. This is interesting. This is
+                        interesting. This is interesting. This is interesting.
+                        This is interesting. This is interesting.
+                    </>
+                }
+            />
+
+            <h2>You can make a difference!</h2>
+            <p>[Insert some info]</p>
+            <p>[Insert some info]</p>
+            <p>[Insert some info]</p>
+            <p>[Insert some info]</p>
+            <p>[Insert some info]</p>
+            <p>[Insert some info]</p>
+            <p>[Insert some info]</p>
+            <p>[Insert some info]</p>
+            <p>[Insert some info]</p>
+            <p>[Insert some info]</p>
+            <p>[Insert some info]</p>
+        </>
+    );
 }
