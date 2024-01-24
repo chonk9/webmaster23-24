e13713df9238bf44c5f8efb9400f6980476a2d91
William <whz11@scasd.org>
2024-01-24 16:32:17 -0500

    sources accordion

---
+++ src/Components/Accordion.js
@@ -0,0 +1,27 @@
+import classNames from 'classnames';
+import React, { useState, useRef } from 'react';
+import '../ComponentStyles/accordion.css';
+
+export default function Accordion({ title, children }) {
+  const content = useRef();
+  const [isActive, setIsActive] = useState(false);
+  return (
+    <div className='accordion'>
+      <div onClick={() => setIsActive(!isActive)} className='accordion-title'>
+        <p>{title}</p>
+        <span>{isActive ? '-' : '+'}</span>
+      </div>
+      <div
+        ref={content}
+        style={
+          isActive ? { height: content.current.scrollHeight } : { height: 0 }
+        }
+        className={classNames('accordion-content', {
+          'accordion-content-active': isActive,
+        })}
+      >
+        <div className='padder'>{children}</div>
+      </div>
+    </div>
+  );
+}
--- src/Pages/Sources.jsx
+++ src/Pages/Sources.jsx
@@ -1,38 +1,26 @@
 import React from 'react'
-
+import Accordion from '../Components/Accordion.js';
 export default function AboutUs() {
   return(<>
-    <h1>Sources.</h1>
-    
-    <div className = 'source-material'>
-      <h2>Research Citations</h2>
-      <div className = 'research'>
-        <li>
-          Research
-        </li>
-        <li>
-          Research
-        </li>
-        <li>
-          Research
-        </li>
+    <h1>Works Cited</h1>
+    <div classname='content'>
+      <h1>Sources</h1>
+      <div id='sources'>
+        <Accordion title='Home Page Videos'>
+        <a href='https://pixabay.com/videos/world-earth-nasa-planet-globe-2/'>
+              https://pixabay.com/videos/world-earth-nasa-planet-globe-2/
+            </a>
+            <p>
+              (Public Domain)
+              <br />
+            </p>
+            <a href='https://pixabay.com/photos/cape-canaveral-florida-space-shuttle-79355/'>
+              https://pixabay.com/photos/cape-canaveral-florida-space-shuttle-79355/
+            </a>
+            <p>(Public Domain)</p>
+        </Accordion>
       </div>
-
-      <div className = 'assets'>
-        <h2>Asset Citations</h2>
-        <li>
-          Asset
-        </li>
-        <li>
-          Asset
-        </li>
-        <li>
-          Asset
-        </li>
-      </div>
-
     </div>
-
   </>);
 
 }
