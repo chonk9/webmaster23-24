49e59b73eb425182c08e99d777721dd6cfc14119
William <whz11@scasd.org>
2024-01-25 23:47:03 -0500

    about us changes

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -4,10 +4,18 @@ export default function AboutUs() {
   return(<>
     <h1>Working for a cleaner future!</h1>
 
-    <h2>At Green Guardians, we're all about clean energy. It's going to be a big part of our future. 
+    <p>
         Our world faces a huge crisis in that we're polluting our planet at a rate that will lead to temperatures rising 
-        by over 1.5°C (2.7°F) by 2050. 
-    </h2>
+        by over <b>1.5°C (2.7°F) by 2050.</b>
+        The ecological damage will cause ice to melt in the arctic, oceans to acidify, temperatures to be more extreme, 
+        storms to be more intense, more droughts and heat waves, and sea levels to displace many people.
+      </p>
+      <p>
+        In the 2015 Paris Agreement, the world agreed on trying to keep temperature increase under 2.0 °C (3.8 °F).
+        However, even if everyone abides by the Paris Agreement, global warming would still reach about 2.7 °C (4.9 °F) by 2100.
+        If we want to reach this goal, we would need to half emissions by the end of this decade and reach net zero emissions by 2050.
+    </p>
+    <h2>At Green Guardians, we're all about clean energy. It's going to be a big part of our future. </h2>
 
   </>);
 
