bb6b6c34030b5decc9a397d6ef7090ee870fd479
chonk9 <qiuquantum00@gmail.com>
2024-01-28 23:38:06 -0500

    changes

--- src/Pages/TaxIncentives.jsx
+++ src/Pages/TaxIncentives.jsx
@@ -32,7 +32,7 @@ export default function TaxIncentives() {
         $3,200 annually on taxes for energy efficient upgrades (through 2032). If you choose to upgrade to heat pump technology, you can 
         save up to $2,000 (30% of cost) on said upgrade. You can also save $1,200 (30% of cost) on other improvements including but not limited to windows, insulation, 
         water heaters, AC systems, and electric panel upgrades. You can claim these credits through&nbsp;
-        <a target='_blank' href='https://www.irs.gov/pub/irs-pdf/f5695.pdf' rel='noreferrer'>IRS Form 5695</a>.
+        <a target='_blank' href='https://www.irs.gov/pub/irs-pdf/f5695.pdf' rel='noreferrer'><b>IRS Form 5695</b></a>.
       </p>
     </div>
     <div class = 'national-table-dropdown'>
