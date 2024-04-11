febbba53bf0a305ed237d93c71175858a23b8c55
William <whz11@scasd.org>
2024-04-10 23:20:23 -0400

    consistency is key

--- src/Pages/LED.jsx
+++ src/Pages/LED.jsx
@@ -14,7 +14,7 @@ export default function CleanSolutionsLed() {
     <div className="content">
       <div>
         <div className="solution">
-          <SolutionHeading visual={ledIconComponent} solutionName="LED Lightbulbs" cost={<Cost value="1.20 per lightbulb per year (plus costs for the lightbulbs)"></Cost>} />
+          <SolutionHeading visual={ledIconComponent} solutionName="LED Lightbulbs" cost={<Cost value="1.26 per lightbulb per year (plus costs for the lightbulbs)"></Cost>} />
           <TextWrap wrappedSide="left" wrappedElt={<>
             <iframe className='youtube-video' width="560" height="315" src="https://www.youtube.com/embed/xanYJ79kjMc?si=8sFty151LkJkAKLU" title="YouTube video player" 
             frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
